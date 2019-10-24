ac7215ad025a198b8efe9253191e5a4934ab412d
xiaobo0903 <xiaobo@cdvcloud.com>
2019-10-24 08:23:45 +0800

    release 3

---
+++ inotify_ts.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+ip=127.0.0.1
+port=80
+#设定本机的IP地址和端口号，供截图服务在调用TS文件截图时使用；
+inotifywait -mrq --timefmt '%d/%m/%y %H:%M'  --format '%T %w %f %e'  --event close_write  --exclude '^(.*\.m3u8|.*\.bak|.*\.jpg)$' /tmp/live/ | awk -F' ' \
+'{len=split($3, array, "/"); \
+cmd= "redis-cli -h localhost set "array[len-2]"_"array[len-1]" http://""'$ip'"":""'$port'""/getts/"array[len-2]"/"array[len-1]"/"$4" ex 10 >/dev/null 2>&1" 
+#print cmd;
+system(cmd);
+#now=systime();print now;
+
+}'
+
+
+
+
+
