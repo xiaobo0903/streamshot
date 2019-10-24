5d94e987a45d4547e293f65b8097c459497e376e
xiaobo0903 <xiaobo@cdvcloud.com>
2019-10-24 17:00:08 +0800

    release 4

--- getshot.lua
+++ getshot.lua
@@ -1,12 +1,50 @@
+--redis服务器的访问地址
+--local ip = "127.0.0.1"
+--local port = 6379
+
+ip = ngx.var.red_ip
+port = ngx.var.red_port+0
+
+--切割字符串
+function mysplit(inputstr, sep)
+        if sep == nil then
+                sep = "%s"
+        end
+        local t={} ; i=1
+        for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+                t[i] = str
+                i = i + 1
+        end
+        return t
+end
+
+--关闭redis数据库
 local function close_redis(red)  
     if not red then  
         return  
     end  
     local ok, err = red:close()  
     if not ok then  
+        ngx.status = 404
         ngx.say("close redis error : ", err)  
+        ngx.exit(404) 
     end  
 end  
+
+--根据ts的访问地址，调用ffmpeg进行截图；
+local function ffmpeg_shot(tsurl, appN, streamN)
+
+    --截图保存临时地址，上传后就会删除，但需要保证Nginx进程需要有读写权限；
+    path='/tmp/'
+    ngx.log(ngx.ERR, "popen begin!")
+    t =io.popen('ffmpeg -y -i '..tsurl..' -f image2 -ss 0 -t 0.001 -vframes 1 '..path..appN..'_'..streamN..'.jpg >/dev/null 2>&1&& redis-cli -h '..ip..' -p '..port..
+    ' -x set '..appN..'_'..streamN..'_pic <'..path..appN..'_'..streamN..'.jpg >/dev/null 2>&1 && redis-cli -h '..ip..' -p '..port..
+    ' expire '..appN..'_'..streamN..'_pic 20 >/dev/null && rm -rf '..path..appN..'_'..streamN..'.jpg&')
+    --ngx.log(ngx.ERR, "popen end1!")
+    --popen系统调用是非阻塞，所以会存在没有截图完成时即返回结果的现像，为了保证在截图后进行后续操作，进程增加了300ms的延时，不够精确，但应该能够满足大部分需求；
+    ngx.sleep(0.3)
+    --ngx.log(ngx.ERR, "sleep end1!")
+end
 
 local function getKey(p_app, p_stream)
 
@@ -14,11 +52,18 @@ local function getKey(p_app, p_stream)
     return redkey
 end
   
---获得url的参数，在nginx接收到请求后，把app和stream都获得后按参数方式放到链接后面；
-local args = ngx.req.get_uri_args()
+--获得uri,uri的
+local uri = ngx.var.uri
 
-local app= args["app"]
-local ch= args["stream"]
+local sp =  mysplit(uri, "/")
+if sp == nil then
+    ngx.status = 404
+    ngx.say("url error!", err)
+    return ngx.exit(404) 
+    
+end
+local app= sp[1]
+local stream= sp[2]
 
 local redis = require("resty.redis")  
   
@@ -26,34 +71,42 @@ local redis = require("resty.redis")
 local red = redis:new()  
 --设置超时（毫秒）  
 red:set_timeout(1000)  
+
 --建立连接  
-local ip = "127.0.0.1"  
-local port = 6379  
 local ok, err = red:connect(ip, port)  
 if not ok then  
+    close_redis(red)  
+    ngx.status = 404
     ngx.say("connect to redis error : ", err)  
-    ngx.exit(404)
-    return close_redis(red)  
+    return ngx.exit(404) 
 end 
 
-ngx.header.content_type = "image/jpeg"
+ngx.header.content_type = "text/plain"
 
---调用API获取数据，因为redis中的value定时时间为30秒，当30秒钟没的接收到新的截图后，则清空记录，所以会出现value不存的错误；  
-local resp, err = red:get(getKey(ch, app))  
-if not resp then  
-    ngx.say("The picture not exit!: ", err)  
-    ngx.exit(404)
-    return close_redis(red)  
+--根据appName和streamName来提取当前ts片的访问地址，然后再进行截图；
+local tsurl, err = red:get(getKey(app, stream))  
+
+if tsurl == ngx.null then  
+    close_redis(red)  
+    ngx.status = 404
+    ngx.say("The tsurl not exit!: ", err)  
+    return ngx.exit(404) 
 end  
 
-close_redis(red)
+ffmpeg_shot(tsurl, app, stream)
 
-if resp == ngx.null then  
+--截图完成后根据appName和streamName对于截图的内容进行提取；
+
+local img, err = red:get(getKey(app, stream.."_pic"))
+close_redis(red)
+if img == ngx.null then  
     
+    ngx.status = 404
     ngx.say("The picture not exit!")  
-    ngx.exit(404)
+    return ngx.exit(404) 
 
 else
-    ngx.say(resp)  
+    ngx.header.content_type = "image/jpeg"
+    ngx.say(img)  
     ngx.exit(200)
 end
